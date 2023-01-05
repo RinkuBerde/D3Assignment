7f04878fe30fa15c23b057455a35ef9dfc9566bf
rinkuberde <rinkuberde@gmail.com>
2023-01-05 12:06:01 +0530

    [rinku]-welcome mes in c#

---
+++ Empoyeewage/Empoyeewage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Empoyeewage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to c#");
+        }
+    }
+}
