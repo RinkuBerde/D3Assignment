e1b73d5f6763020088999cca2c644175d25c9d5d
rinkuberde <rinkuberde@gmail.com>
2023-01-05 15:32:53 +0530

    [rinku] employee wage using switch case

--- Empoyeewage/Empoyeewage/Program.cs
+++ Empoyeewage/Empoyeewage/Program.cs
@@ -1,37 +1,42 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Empoyeewage
 {
-    internal class PartTimeEmpWage
+    internal class EmpWageSwitchCase
     {
-        static void Main(string[] args)
+        
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public static void Main(string[] args)
         {
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            //Variables
+           
+            //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            //computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp wage : " + empWage);
-
-
+            Console.WriteLine("Emp Wage: " + empWage);
         }
+
+
+
+
+
+
     }
 }
