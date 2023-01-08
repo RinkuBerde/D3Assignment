33026be53bba2fc3d1ad1eb0ff5b22dbcf796af3
rinkuberde <rinkuberde@gmail.com>
2023-01-09 00:11:59 +0530

    [Rinku]- Emp wage for multiple compannies

--- Empoyeewage/Empoyeewage/Program.cs
+++ Empoyeewage/Empoyeewage/Program.cs
@@ -7,17 +7,14 @@ namespace Empoyeewage
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
-        public static int computeEmpwage()
+        public static int computeEmpwage(string company, int empRatePerHour, int numofWorkingDays, int maxHoursPerMonth)
         {
+            //variable
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
 
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numofWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -37,15 +34,15 @@ namespace Empoyeewage
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
             Console.WriteLine("Total Emp wage: " + totalEmpWage);
             return totalEmpWage;
         }
-
+        
         public static void Main(string[] args)
         {
-            computeEmpwage();
-
+            computeEmpwage("DMart",20,20,100);
+            computeEmpwage("Reliance", 10, 4, 20);
         }
     }
 }
