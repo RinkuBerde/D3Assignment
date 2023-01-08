e4576f66c21712ba060c47dd28bf9570b536b5eb
rinkuberde <rinkuberde@gmail.com>
2023-01-08 21:44:30 +0530

    [Rinku]- EmpWage using class method

--- Empoyeewage/Empoyeewage/Program.cs
+++ Empoyeewage/Empoyeewage/Program.cs
@@ -8,18 +8,16 @@ namespace Empoyeewage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
 
-        public static void Main(string[] args)
+        public static int computeEmpwage()
         {
-
-            //variables
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
 
-            while(totalEmpHrs <= MAX_HRS_IN_MONTH  && totalWorkingDays < NUM_OF_WORKING_DAYS )
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -41,7 +39,16 @@ namespace Empoyeewage
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp wage: " + totalEmpWage);
+            return totalEmpWage;
         }
+
+        public static void Main(string[] args)
+        {
+            computeEmpwage();
+
+        }
+    }
 }
-}
+
+
 
