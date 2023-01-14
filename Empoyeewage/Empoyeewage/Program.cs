2ef8dbc1174f32c391f0d499c0980f0dfdc74ea7
rinkuberde <rinkuberde@gmail.com>
2023-01-14 17:36:37 +0530

    [Rinku] - List-Of-Muiltiple-Companies

--- Empoyeewage/Empoyeewage/Program.cs
+++ Empoyeewage/Empoyeewage/Program.cs
@@ -7,27 +7,35 @@ namespace Empoyeewage
     {   
         public static void Main(string[] args)
         {
-           //UC-9 Total Wage for Each Company
-            UC9EmpWageObject dmart = new UC9EmpWageObject("Dmart", 20, 20, 100);
-            UC9EmpWageObject reliance = new UC9EmpWageObject("Reliance", 20, 23, 210);
-            dmart.computeEmpWage();
-            Console.Write(dmart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
+            /*  //UC-9 Total Wage for Each Company
+               UC9EmpWageObject dmart = new UC9EmpWageObject("Dmart", 20, 20, 100);
+               UC9EmpWageObject reliance = new UC9EmpWageObject("Reliance", 20, 23, 210);
+               dmart.computeEmpWage();
+               Console.Write(dmart.toString());
+               reliance.computeEmpWage();
+               Console.WriteLine(reliance.toString());
 
-            //UC-10 manage Employee Wage of multiple companies
+               //UC-10 manage Employee Wage of multiple companies
+               EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+               empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 100);
+               empWageBuilder.addCompanyEmpWage("Reliance", 10, 30, 200);
+               empWageBuilder.computeEmpWage();
+
+               //UC-11 Employee Wage of multiple companies using API approach
+               EmpWageBuilder1 empWageBuilder1 = new EmpWageBuilder1();
+               empWageBuilder1.addCompanyEmpWage("DMart", 20, 25, 100);
+               empWageBuilder1.addCompanyEmpWage("Reliance", 10, 40, 210);
+               empWageBuilder1.computeEmpWage();
+               Console.WriteLine("total wage : " + empWageBuilder1.getTotalWage("Dmart"));
+               Console.WriteLine("total wage : " + empWageBuilder1.getTotalWage("Dmart"));*/
+
+            //UC-12 list of multiple companies
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 100);
-            empWageBuilder.addCompanyEmpWage("Reliance", 10, 30, 200);
-            empWageBuilder.computeEmpWage();
+            empWageBuilder.AddCompanyEmpWage("Reliance", 20, 10, 100);
+            empWageBuilder.AddCompanyEmpWage("Dmart", 20, 20, 110);
+            empWageBuilder.ComputeEmpWage();
 
-            //UC-11 Employee Wage of multiple companies using API approach
-            EmpWageBuilder1 empWageBuilder1 = new EmpWageBuilder1();
-            empWageBuilder1.addCompanyEmpWage("DMart", 20, 25, 100);
-            empWageBuilder1.addCompanyEmpWage("Reliance", 10, 40, 210);
-            empWageBuilder1.computeEmpWage();
-            Console.WriteLine("total wage : " + empWageBuilder1.getTotalWage("Dmart"));
-            Console.WriteLine("total wage : " + empWageBuilder1.getTotalWage("Dmart")); 
+
 
 
 
---
+++ Empoyeewage/Empoyeewage/UC12_ListComapnies.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empoyeewage
+{
+    interface IEmployeeWage
+    {
+        void ComputeEmpWage();
+        void AddCompanyEmpWage(string company, int wagePerHour, int numWorkingDays, int maxHoursPerMonth);
+    }
+
+
+    public class EmpWageBuilderArray : IEmployeeWage
+    {/// <summary>
+     /// storing company employee wage in a list instead of array
+     /// </summary>
+        private int numOfCompany = 0;
+        private LinkedList<CompanyEmpWage> companyEmpWageList;
+
+        public EmpWageBuilderArray()
+        {
+            //initilizing linked list
+            this.companyEmpWageList = new LinkedList<CompanyEmpWage>();
+        }
+
+        public void AddCompanyEmpWage(string company, int wagePerHour, int numWorkingDays, int maxHoursPerMonth)
+        {
+            //calling Companywage arguments with object and adding to list
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, wagePerHour, numWorkingDays, maxHoursPerMonth);
+            this.companyEmpWageList.AddLast(companyEmpWage);
+        }
+
+        public void ComputeEmpWage()
+        {
+            //computing emp wage and adding it to total emp wage
+            //and displaying result
+            foreach (CompanyEmpWage companyEmpWage in this.companyEmpWageList)
+            {
+                companyEmpWage.SetTotalEmpWage(this.ComputeEmpWage(companyEmpWage));
+                Console.WriteLine(companyEmpWage.Result());
+            }
+        }
+
+        private int ComputeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            //constants
+            const int IS_FULL_TIME = 8;
+            const int IS_PART_TIME = 4;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            //variables
+            int dailyWage = 0;
+            int days, isPresent;
+            // int totalWage = 0;
+            int totalHours = 0;
+            //random number generation
+            Random rand = new Random();
+            //calculating for month
+            for (days = 1; days <= companyEmpWage.numWorkingDays; days++) // calculating for 20 working days
+            {
+                isPresent = rand.Next(0, 3);
+                //using switch case
+                switch (isPresent)
+                {
+                    case FULL_TIME: // Employee is present full time
+                        {
+                            dailyWage = companyEmpWage.wagePerHour * IS_FULL_TIME;
+                            break;
+                        }
+                    case PART_TIME: //employee is present for part time
+                        {
+                            dailyWage = companyEmpWage.wagePerHour * IS_PART_TIME;
+                            break;
+                        }
+                    default: // employee is absent
+                        {
+                            dailyWage = isPresent;
+                            break;
+                        }
+                }
+                //checking total number of hours
+                totalHours += dailyWage / 20; //calculate total hours worked
+                companyEmpWage.totalWage += dailyWage; // calculating montly wage
+                if (totalHours >= companyEmpWage.maxHoursPerMonth) //maximum total hours 
+                    break;
+                Console.WriteLine(" Day#: " + days + " Emp Hrs : " + totalHours);
+            }
+            return totalHours * companyEmpWage.wagePerHour;
+        }
+
+
+
+    }
+}
